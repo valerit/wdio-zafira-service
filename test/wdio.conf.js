c8802e7877885f22b8408a50cd5cde20c7d8025e
valeri <valeritsert@gmail.com>
2019-02-07 09:21:51 +0400

    rename test configs

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -13,13 +13,13 @@ exports.config = {
       refreshToken: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwicGFzc3dvcmQiOiIveU45VDZZaHY5ZHRCeDNmSUFZSmxqeG12YzRObGhrMCIsInRlbmFudCI6InphZmlyYSIsImV4cCI6MTMwMzk3OTc0MzUwfQ.DuKkYg4FnU1Knyas7-YRF-wNk_Uv5wmRqmds44Z134r7VDvyoPr2KZmYZuu5dQIgErfyV4aN0e5zYgWGEebpUg',
       username: 'admin',
       testSuite: {
-        fileName: 'valeri.xml',
-        name: 'valeri_test',
+        fileName: 'test.xml',
+        name: 'example_test',
       },
       job: { // Jenkins Settings
         "jenkinsHost": process.env.HOST || 'demo.qaprosoft.com',
-        "jobURL": process.env.BUILD_URL || 'http://demo.qaprosoft.com/jenkins/job/valeridemo/5/', //  // Jenkins Build URL
-        "name": process.env.JOB_NAME || 'valeridemo',
+        "jobURL": process.env.BUILD_URL || 'http://demo.qaprosoft.com/jenkins/job/sharmademo/6/', //  // Jenkins Build URL
+        "name": process.env.JOB_NAME || 'example',
       },
       run: {
         buildNumber: process.env.BUILD_NUMBER || 6,
